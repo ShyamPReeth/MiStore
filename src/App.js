04878bbe5b7bde6f8227d291ccc1a016a356a5bb
Shyam <shyampreeth2002@gmail.com>
2023-03-29 20:49:46 +0530

    MI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import PreNavbar from './components/PreNavbar';
+import Navbar from "./components/Navbar.js"
+import {  BrowserRouter as Router,Route, Routes } from "react-router-dom"
+import Slider from "./components/Slider.js"
+import data from "./data/data.json"
+import Offers from "./components/Offers.js"
+import Heading  from "./components/Heading.js"
+import StarProduct from "./components/StarProduct.js"
+import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.js"
+import HotAccessories from "./components/HotAccessories.js"
+import ProductReviews from "./components/ProductReviews.js"
+import Videos from "./components/Videos.js"
+import Banner from "./components/Banner.js"
+import Footer from "./components/Footer.js"
+import NavOptios from "./components/NavOptios.js"
+import { Fragment } from 'react';
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <PreNavbar/>
+      <Navbar />
+      <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} accessories={data.accessories} audio={data.audio} />
+      <Slider start={data.banner.start}/>
+      <Offers offer={data.offer}/>
+      <Heading text={"STAR PRODUCTS"} />
+      <StarProduct starProduct={data.starProduct}/>
+      <Heading text={"HOT ACCESSORIES"} />
+      <HotAccessoriesMenu />
+      <Routes>  
+      <Route exact path='/music' element={<HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music} />}></Route>
+      <Route exact path='/smartDevice' element={<HotAccessories music={data.hotAccessories.smartDevice} musicCover={data.hotAccessoriesCover.smartDevice} />}></Route>
+      <Route exact path='/home' element={<HotAccessories music={data.hotAccessories.home} musicCover={data.hotAccessoriesCover.home} />}></Route>
+      <Route exact path='/lifestyle' element={<HotAccessories music={data.hotAccessories.lifeStyle} musicCover={data.hotAccessoriesCover.lifeStyle} />}></Route>
+      <Route exact path='/mobileAccessories' element={<HotAccessories music={data.hotAccessories.mobileAccessories} musicCover={data.hotAccessoriesCover.mobileAccessories} />}></Route>
+      </Routes>
+      <Heading text="PRODUCT REVIEWS"/>
+      <ProductReviews productReviews={data.productReviews}/>
+      <Heading text="VIDEOS"/>
+      <Videos videos={data.videos} />
+      <Heading text="IN THE PRESS" />
+      <Banner banner={data.banner} />
+      <Footer footer={data.footer}/>
+    </Router>
+
+
   );
 }
 
